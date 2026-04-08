b099da69eca65e23335852caab8fb5b9bc636e9c
Freecell <232599524+FreecellRaid@users.noreply.github.com>
2026-04-08 23:46:18 +0800

    feat: initial release of Freecell Log Studio

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base:'/Freecell-log-studio/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
